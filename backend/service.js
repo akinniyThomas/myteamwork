7a249ce73bf1793c6b489845bfd0e90d2cfb9610
akinniyThomas <akinniyithomas@yahoo.com>
2019-10-31 12:11:14 +0100

    Added the service to start server

--- backend/service.js
+++ backend/service.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
-    res.send("Hello There!!!");
+    res.send("Hello There SWAG for me!!!");
 });
 
 app.listen(port, () => console.log("Service Running"));
